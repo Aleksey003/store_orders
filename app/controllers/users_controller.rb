00bd53650a2eaa685923c8365b20b585bff7aea2
Aleksey <alekseylks@gmail.com>
2013-03-08 22:00:39 +0200

    sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,7 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(params[:user])
+		@user.parent = User.find(session[:user_id])
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
