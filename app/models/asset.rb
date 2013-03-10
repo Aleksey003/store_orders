15df20f3e0977a05ad597564cc3b17a5f302b1f6
Aleksey <alekseylks@gmail.com>
2013-03-11 01:52:35 +0200

    Assets product

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,6 @@
 class Asset < ActiveRecord::Base
   belongs_to :product
-  attr_accessible :caption
-  has_attached_file :data, :styles => { :small => “150×150>”, :large => “320×240>” }
+  attr_accessible :caption, :data
+  has_attached_file :data, :styles => { :medium => "300x300>", :thumb => "50x50>" }
 
 end
